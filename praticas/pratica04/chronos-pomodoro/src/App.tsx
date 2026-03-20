23f4133b678506222e3e20953b1f477d94480ceb
commit-ana <ana.lima25@iesb.edu.br>
2026-03-19 21:40:10 -0300

    Arquivos atualizados até a prática02

---
+++ praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -0,0 +1,6 @@
+export function App() {
+  <>
+  <h1>Bem vindo a primeira aula de React!</h1>,
+  <h2>Nossa aula de introdução!</h2>
+ </>;
+}
---
+++ praticas/pratica04/chronos-pomodoro/src/main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+createRoot(document.getElementById("root")!).render(
+
+  console.log("Olá mundo!"),
+createRoot(document.getElementById("root")!).render(
+
+  <>
+  <h1>Bem vindo a primeira aula de React!</h1>,
+  <h2>Nossa aula de introdução!</h2>
+ </>,
+);

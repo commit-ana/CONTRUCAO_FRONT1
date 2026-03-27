50c83d7bb8f63a0240329a78507a3a43555d953d
commit-ana <ana.lima25@iesb.edu.br>
2026-03-27 19:47:51 -0300

    pratica14 - formulario base funcionando

--- praticas/pratica04/chronos-pomodoro/src/App.tsx
+++ praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -2,7 +2,9 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+
 import './styles/theme.css';
+import './styles/globals.css';
 
 export function App() {
   return (
@@ -14,11 +16,38 @@ export function App() {
       <Container>
         <Menu />
       </Container>
+
        <Container>
         <CountDown />
       </Container>
+
+      <Container>
+  <form className="form" action="">
+    {/* Grupo 1 */}
+    <div className="formRow">
+      <label htmlFor="meuInput">task</label>
+      <input id="meuInput" type="text" />
+    </div>
+
+    {/* Grupo 2 */}
+    <div className="formRow">
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+
+    {/* Grupo 3 */}
+    <div className="formRow">
+      <p>Ciclos</p>
+      <p>0 0 0 0 0 0 0</p>
+    </div>
+
+    {/* Grupo 4 */}
+    <div className="formRow">
+      <button>Enviar</button>
+    </div>
+  </form>
+</Container>
     </>
-
-
   );
 }
+
+

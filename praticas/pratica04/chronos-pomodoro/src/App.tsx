6ba00ac9c89ef74167aa290370077288871ce2b5
commit-ana <ana.lima25@iesb.edu.br>
2026-03-27 19:57:34 -0300

    pratica15 - componente DefaultInput criado

--- praticas/pratica04/chronos-pomodoro/src/App.tsx
+++ praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -2,6 +2,7 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput'
 
 import './styles/theme.css';
 import './styles/globals.css';
@@ -49,5 +50,4 @@ export function App() {
     </>
   );
 }
-
-
+<DefaultInput id="meuInput" type="text" />

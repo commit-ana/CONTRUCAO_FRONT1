5e82c00c862576bb93b16e193e060435cecbf6a1
commit-ana <ana.lima25@iesb.edu.br>
2026-03-27 21:09:08 -0300

    pratica20 - footer e finalizacao do layout

--- praticas/pratica04/chronos-pomodoro/src/App.tsx
+++ praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -6,6 +6,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/globals.css';
@@ -26,44 +27,40 @@ export function App() {
       </Container>
 
       <Container>
-  <form className="form" action="">
-    {/* Grupo 1 */}
-    <div className="formRow">
-      <label htmlFor="meuInput">task</label>
-      <input id="meuInput" type="text" />
-    </div>
+  <form className="form">
+  <div className="formRow">
+    <DefaultInput
+      id="meuInput"
+      type="text"
+      labelText="task"
+      placeholder="Digite algo"
+    />
+  </div>
 
-    {/* Grupo 2 */}
-    <div className="formRow">
-      <p>Lorem ipsum dolor sit amet.</p>
-    </div>
+  <div className="formRow">
+    <p>Lorem ipsum dolor sit amet.</p>
+  </div>
 
-    {/* Grupo 3 */}
-    <div className="formRow">
-      <Cycles />
-    </div>
+  <div className="formRow">
+    <Cycles />
+  </div>
 
-    {/* Grupo 4 */}
-    <div className="formRow">
-      <button>Enviar</button>
-    </div>
-  </form>
-</Container>
+
 
 <div className="formRow">
   <DefaultButton icon={<PlayCircleIcon />} />
   <DefaultButton icon={<StopCircleIcon />} color="red" />
 </div>
+</form>
+</Container>
 
-<DefaultInput
-  id="meuInput"
-  type="text"
-  labelText="task"
-  placeholder="Digite algo"
-  required
-/>
+<Container>
+  <Footer />
+</Container>
+
 </>
-    
   );
 }
+
+
 

bad9c56d385d6edfef06026175c5f774666d8f5e
commit-ana <ana.lima25@iesb.edu.br>
2026-05-12 16:04:48 -0300

    pratica26 - define variáveis CSS para os temas claro e escuro

--- praticas/pomodoro/src/components/Menu/index.tsx
+++ praticas/pomodoro/src/components/Menu/index.tsx
@@ -1,38 +1,32 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react'; 
 
 type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
   const [theme, setTheme] = useState<AvailableThemes>('dark');
 
+  
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
     event.preventDefault();
-
-    // 1. Lógica de inversão de tema usando callback (Jeito Seguro)
-    setTheme((prevTheme) => {
-      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
-      
-      // ❌ NÃO FAÇA ISSO: document.documentElement.setAttribute('data-theme', nextTheme);
-      // Alterar o DOM aqui dentro é uma má prática pois esta função deve ser pura.
-      
-      return nextTheme;
-    });
-
-    // ❌ SE FIZER ISSO AQUI: document.documentElement.setAttribute('data-theme', theme);
-    // O valor de 'theme' ainda será o ANTIGO, por isso o tema fica atrasado um clique.
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
   }
+
+  useEffect(() => {
+
+    document.documentElement.setAttribute('data-theme', theme);
+
+    return () => {
+      console.log('Limpando efeito colateral anterior...');
+    };
+  }, [theme]); 
 
   return (
     <nav className={styles.menu}>
-      {/* Visualização para teste */}
-      <span style={{ color: 'var(--gray-800)', fontSize: '12px' }}>
-        Estado: {theme}
-      </span>
-
+     
       <a className={styles.menuLink} href='#' aria-label='Ir para a Home' title='Ir para a Home'>
         <HouseIcon />
       </a>

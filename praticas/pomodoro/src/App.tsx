739fdb62328fefa4516d97a6abcb1aefb3175811
commit-ana <ana.lima25@iesb.edu.br>
2026-05-12 17:34:42 -0300

    pratica29 - cria componente GenericHtml para estilização automática de textos e adiciona página Sobre

--- praticas/pomodoro/src/App.tsx
+++ praticas/pomodoro/src/App.tsx
@@ -1,6 +1,6 @@
 import { Home } from './pages/Home';
 import './styles/theme.css';
-import './styles/globals.css'; 
+import './styles/globals.css'; // verifique se é global.css ou globals.css no seu projeto
 
 export function App() {
   return <Home />;

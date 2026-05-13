d768d2f1bea05c8388516054fcfd06e64b3d0333
commit-ana <ana.lima25@iesb.edu.br>
2026-05-13 15:52:20 -0300

    pratica39 - implementa captura de dados do formulário usando useRef

--- praticas/pomodoro/src/components/MainForm/index.tsx
+++ praticas/pomodoro/src/components/MainForm/index.tsx
@@ -1,28 +1,39 @@
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { PlayCircleIcon, StopCircle, StopCircleIcon } from 'lucide-react';
+import { useRef } from 'react'; // 1. Importando o useRef
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
+  const { state } = useTaskContext();
+
+  // 2. Criando a "caixa forte" para o input
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    console.log('DEU CERTO');
+    // 4. Lendo o valor apenas agora, no submit!
+    const taskTitle = taskNameInput.current?.value;
+    console.log('Tarefa capturada:', taskTitle);
   }
 
   return (
-    <form onSubmit={handleCreateNewTask} className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form'>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
           id='meuInput'
           type='text'
-          placeholder='Digite algo'
+          placeholder='Qual a sua tarefa?'
+          // 3. Passando a referência para o nosso componente
+          ref={taskNameInput}
         />
       </div>
 
       <div className='formRow'>
-        <p>Próximo intervalo é de 25min</p>
+        <p>Próximo intervalo é de {state.config.workTime}min</p>
       </div>
 
       <div className='formRow'>
@@ -33,6 +44,8 @@ export function MainForm() {
         <DefaultButton icon={<PlayCircleIcon />} />
         <DefaultButton icon={<StopCircleIcon />} color='red' />
       </div>
+
+      
     </form>
   );
 }

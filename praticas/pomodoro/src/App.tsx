5f9ed8bbe66f58e5b4405fea78f5852828604c7c
commit-ana <ana.lima25@iesb.edu.br>
2026-05-18 09:31:25 -0300

    pratica 64 - isola roteamento no MainRouter e implementa ScrollToTop suave

--- praticas/pomodoro/src/App.tsx
+++ praticas/pomodoro/src/App.tsx
@@ -1,25 +1,14 @@
-import { Home } from './pages/Home';
-import './styles/theme.css';
-import './styles/globals.css';
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { MessagesContainer } from './components/MessagesContainer';
-import { BrowserRouter, Route, Routes } from 'react-router';
-import { NotFound } from './pages/NotFound';
-import { AboutPomodoro } from './pages/AboutPomodoro';
+import { MainRouter } from './routers/MainRouter';
+import './styles/theme.css';
+import './styles/globals.css';
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
-
-            {/* O asterisco é a rota coringa: se o usuário digitar um endereço que não existe, cai aqui */}
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
---
+++ praticas/pomodoro/src/routers/MainRouter/index.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
+import { AboutPomodoro } from '../../pages/AboutPomodoro';
+import { NotFound } from '../../pages/NotFound';
+import { Home } from '../../pages/Home';
+import { useEffect } from 'react';
+
+// ⬆️ Esse é o truque! Um componente que não desenha nada na tela (retorna null),
+// mas que fica "espiando" a URL. Toda vez que a URL muda, ele joga a tela pro topo!
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [pathname]);
+
+  return null;
+}
+
+export function MainRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <ScrollToTop />
+    </BrowserRouter>
+  );
+}

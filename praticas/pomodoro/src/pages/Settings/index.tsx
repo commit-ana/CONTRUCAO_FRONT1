015c0ad3ae74e637931e4ad65f0c4b3a9429ebba
commit-ana <ana.lima25@iesb.edu.br>
2026-05-20 11:12:14 -0300

    pratica 78 - cria estrutura da pagina de configuracoes e adiciona rota

--- praticas/pomodoro/src/pages/Settings/index.tsx
+++ praticas/pomodoro/src/pages/Settings/index.tsx
@@ -4,65 +4,8 @@ import { DefaultButton } from '../../components/DefaultButton';
 import { DefaultInput } from '../../components/DefaultInput';
 import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../components/templates/MainTemplate';
-import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
-import { useEffect, useRef } from 'react';
-import { showMessage } from '../../adapters/showMessage';
-import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 
 export function Settings() {
-  const { state, dispatch } = useTaskContext();
-  const workTimeInput = useRef<HTMLInputElement>(null);
-  const shortBreakTimeInput = useRef<HTMLInputElement>(null);
-  const longBreakTimeInput = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    document.title = 'Configurações - Chronos Pomodoro';
-  }, []);
-
-  function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    showMessage.dismiss();
-
-    const formErrors = [];
-
-    const workTime = Number(workTimeInput.current?.value);
-    const shortBreakTime = Number(shortBreakTimeInput.current?.value);
-    const longBreakTime = Number(longBreakTimeInput.current?.value);
-
-    if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
-      formErrors.push('Digite apenas números para TODOS os campos');
-    }
-
-    if (workTime < 1 || workTime > 99) {
-      formErrors.push('Digite valores entre 1 e 99 para foco');
-    }
-
-    if (shortBreakTime < 1 || shortBreakTime > 30) {
-      formErrors.push('Digite valores entre 1 e 30 para descanso curto');
-    }
-
-    if (longBreakTime < 1 || longBreakTime > 60) {
-      formErrors.push('Digite valores entre 1 e 60 para descanso longo');
-    }
-
-    if (formErrors.length > 0) {
-      formErrors.forEach(error => {
-        showMessage.error(error);
-      });
-      return;
-    }
-
-    dispatch({
-      type: TaskActionTypes.CHANGE_SETTINGS,
-      payload: {
-        workTime,
-        shortBreakTime,
-        longBreakTime,
-      },
-    });
-    showMessage.success('Configurações salvas');
-  }
-
   return (
     <MainTemplate>
       <Container>
@@ -70,40 +13,21 @@ export function Settings() {
       </Container>
 
       <Container>
-        <p style={{ textAlign: 'center' }}>
-          Modifique as configurações para tempo de foco, descanso curso e
-          descanso longo.
+        <p style={{ textAlign: 'center', fontSize: '1.6rem', color: 'var(--text-default)' }}>
+          Modifique as configurações para tempo de foco, descanso curto e descanso longo.
         </p>
       </Container>
 
       <Container>
-        <form onSubmit={handleSaveSettings} action='' className='form'>
+        <form action='' className='form'>
           <div className='formRow'>
-            <DefaultInput
-              id='workTime'
-              labelText='Foco'
-              ref={workTimeInput}
-              defaultValue={state.config.workTime}
-              type='number'
-            />
+            <DefaultInput id='workTime' labelText='Foco' />
           </div>
           <div className='formRow'>
-            <DefaultInput
-              id='shortBreakTime'
-              labelText='Descanso curto'
-              ref={shortBreakTimeInput}
-              defaultValue={state.config.shortBreakTime}
-              type='number'
-            />
+            <DefaultInput id='shortBreakTime' labelText='Descanso curto' />
           </div>
           <div className='formRow'>
-            <DefaultInput
-              id='longBreakTime'
-              labelText='Descanso longo'
-              ref={longBreakTimeInput}
-              defaultValue={state.config.longBreakTime}
-              type='number'
-            />
+            <DefaultInput id='longBreakTime' labelText='Descanso longo' />
           </div>
           <div className='formRow'>
             <DefaultButton
